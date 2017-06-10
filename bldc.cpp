542c5e8ce5e292f60fb55afe53e3b41246c0281d
wassfila <wassim.filali@gmail.com>
2017-06-10 19:58:04 +0200

    center alligned mode

--- bldc.cpp
+++ bldc.cpp
@@ -15,6 +15,8 @@ bldc::bldc(Serial *ps,PinName v_pwm1,PinName v_pwm2,PinName v_pwm3,PinName v_en)
         pwm2 = 0;
         pwm3 = 0;
 
+        TIM1->CR1 &= ~TIM_CR1_CMS_Msk;  //clear CMS
+        TIM1->CR1 |= TIM_CR1_CMS_1;     //set CMS to 01
         //Prescaler : no effect
         TIM1->PSC = 0;
         TIM1->ARR = 3200;//50 us
@@ -29,19 +31,18 @@ void bldc::setVector(int angle,float norm)
 {
         //----------------------------------------------------------measure pulse start
         int a1 = angle % 256;
-        uint16_t pwm1_buf = sin_Table[a1];
+        uint16_t pwm1_buf = norm * sin_Table[a1];
         int a2 = a1 + 85;
         if(a2>=256)a2-=256;
-        uint16_t pwm2_buf = sin_Table[a2];
+        uint16_t pwm2_buf = norm * sin_Table[a2];
         int a3 = a1 + 170;
         if(a3>=256)a3-=256;
-        uint16_t pwm3_buf = sin_Table[a3];
+        uint16_t pwm3_buf = norm * sin_Table[a3];
         //----------------------------------------------------------measure pulse stop => 67.75 us
 
-        /*ser->printf("pwm1 : %u\n",pwm1_buf);
-        ser->printf("pwm2 : %u\n",pwm2_buf);
-        ser->printf("pwm3 : %u\n",pwm3_buf);
-        ser->printf("\n");*/
+        /*ser->printf("pwm1 : %u - ",pwm1_buf);
+        ser->printf("pwm2 : %u - ",pwm2_buf);
+        ser->printf("pwm3 : %u\n",pwm3_buf);*/
         //pwm1.pulsewidth_us(100);
         //pwm2.pulsewidth_us(200);
 
